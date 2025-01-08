90926f025179402b4a24c608bc78315b0a3041b1
sobir-swe <sobirjon.swe@gmail.com>
2025-01-08 12:54:04 +0500

    chore: update routes for new file management features

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FileController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/files', [FileController::class, 'index'])->name('files.index');
+    Route::get('/files/create', [FileController::class, 'create'])->name('files.create');
+    Route::post('/files', [FileController::class, 'store'])->name('files.store');
+    Route::get('/files/{id}', [FileController::class, 'show'])->name('files.show');
+    Route::get('/files/{id}/edit', [FileController::class, 'edit'])->name('files.edit');
+    Route::put('/files/{id}', [FileController::class, 'update'])->name('files.update');
+    Route::delete('/files/{id}', [FileController::class, 'destroy'])->name('files.destroy');
 });
 
 require __DIR__.'/auth.php';
