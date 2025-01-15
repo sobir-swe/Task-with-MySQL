28ec41aec785edd3ab1d0059aa77e10e685c6f6d
sobir-swe <sobirjon.swe@gmail.com>
2025-01-15 19:54:37 +0500

    refactor: modify AppServiceProvider configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Service\Sessions;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $account = Sessions::GetSession();
+        View::share('account', $account);
     }
 }
